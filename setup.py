7978beb3846120b4984f65ae24d05c20b3f21537
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2018-07-05 14:37:49 -0400

    fixing up installation

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup, find_packages
 
-requires = [
-    'lxml',
-    'requests',
-    'encrypt'
-]
-
 setup(name='mano',
       description='Mano - API for Beiwe Research Platform',
       author='Neuroinformatics Research Group',
       author_email='info@neuroinfo.org',
       packages=find_packages(),
-      url='http://neuroinformatics.harvard.edu/',
-      install_requires=requires
+      url='http://neuroinformatics.harvard.edu/'
     )
 
