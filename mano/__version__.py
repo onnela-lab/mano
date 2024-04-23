383f0652f80970ba44dfc356031b1bb49da777d4
Eli Jones <biblicabeebli@gmail.com>
2024-04-23 12:15:43 -0400

    [cleanup-refactors] makes the file names... names while (attempting) to preserve compatibility with old imports.

--- mano/__version__.py
+++ mano/__version__.py
@@ -1,7 +1,6 @@
 __title__ = 'mano'
 __description__ = 'Python API for Beiwe Research Platform'
 __url__ = 'https://www.neuroinfo.org'
-__version__ = '0.5.2'
-__author__ = 'Neuroinformatics Research Group'
-__author_email__ = 'info@neuroinfo.org'
-
+__version__ = '0.5.3'
+__author__ = 'Onnela Lab'
+__author_email__ = 'biblicabeebli@gmail.com'
