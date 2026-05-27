69d6635f728bfb4b4f8ed5dcc4188068a907e2d3
Eli Jones <biblicabeebli@gmail.com>
2026-05-27 14:42:54 -0400

    [minor-improvements] changes a value that was erroniously named in upper case.

--- mano/constants.py
+++ mano/constants.py
@@ -15,7 +15,7 @@ class GlobalSettings:
     skip_user_interaction: bool = True   # main() in CLI sets this to False when appropriate.
     multithreading_count: int = 0        # zero or negative means use the number of CPU cores.
     
-    BACKFILL_WINDOW = 20
+    backfill_window = 20
 
 
 #
